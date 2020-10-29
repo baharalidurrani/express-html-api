5208fa7640b5cc40e7477f6d19f782bf70fa473e
loralridz <noorrida2000@gmail.com>
2020-10-29 22:33:11 +0500

    Added files

--- client/index.js
+++
@@ -1,8 +0,0 @@
-console.log("hello from client script", "color: #007acc;");
-
-async function main() {
-  const res = await fetch("api/current");
-  const jsonData = await res.json();
-  console.log(jsonData);
-}
-main();
--- server.js
+++ server.js
@@ -2,32 +2,56 @@
 const express = require("express");
 const path = require("path");
 const nFetch = require("node-fetch");
+var bodyParser = require('body-parser');
 
 const app = express();
 const port = 3000;
 
 app.use(express.static(__dirname + "/client"));
 
+app.set('views', __dirname + '/views');
+app.set('view engine', 'ejs');
+
+//body parser
+var bodyParser = require('body-parser');
+var urlencodedParser = bodyParser.urlencoded({ extended: false })
+
 app.get("/", (req, res) => {
-  res.sendFile(path.join(__dirname + "client" + "index.html"));
+    res.render("index.ejs", {});
 });
 
-app.get("/api/current", async (req, res) => {
-  const endpoint = `https://rest.coinapi.io/v1/exchangerate/BTC/USD`;
-  const period = `1DAY`;
-  const latest = `https://rest.coinapi.io/v1/ohlcv/BITSTAMP_SPOT_BTC_USD/latest?period_id=${period}`;
-  const historic = `https://rest.coinapi.io/v1/ohlcv/BTC/USD/history?period_id=${period}&time_start=2016-01-01T00:00:00`;
 
-  const headers = {
-    "X-CoinAPI-Key": "7922AEFB-B522-4E06-9720-2298F282EA12"
-  };
+app.get("/api/current", async(req, res) => {
+    const endpoint = `https://rest.coinapi.io/v1/exchangerate/BTC/USD`;
+    const period = `1DAY`;
+    const latest = `https://rest.coinapi.io/v1/ohlcv/BITSTAMP_SPOT_BTC_USD/latest?period_id=${period}`;
+    const historic = `https://rest.coinapi.io/v1/ohlcv/BTC/USD/history?time_start=${'2016-01-01T00:00:00'}&time_end=${'2016-01-01T00:00:00'}`;
 
-  const apiRes = await nFetch(latest, { headers });
-  const jsonData = await apiRes.json();
+    const headers = {
+        "X-CoinAPI-Key": "7922AEFB-B522-4E06-9720-2298F282EA12"
+    };
 
-  res.send(jsonData);
+    const apiRes = await nFetch(latest, { headers });
+    const jsonData = await apiRes.json();
+    // console.log(jsonData)
+    res.send(jsonData);
 });
+
+
+app.post("/api/current/post", urlencodedParser, async(req, res) => {
+
+    const data = fetch(`https://rest.coinapi.io/v1/ohlcv/BTC/USD/history?time_start=${req.body.start}&time_end=${req.body.end}`, {
+        method: 'POST', // or 'PUT'
+        headers: {
+            "X-CoinAPI-Key": "7922AEFB-B522-4E06-9720-2298F282EA12"
+        },
+        body: JSON.stringify(data),
+    })
+    const jsonData = await apiRes.json();
+    console.log(jsonData);
+
+})
 
 app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`);
+    console.log(`Example app listening at http://localhost:${port}`);
 });
