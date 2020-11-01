97b19efa8ae6b1bf2d8db687a5f32a8065673300
Durrani <baharalidurrani@gmail.com>
2020-11-01 22:27:57 +0500

    backend try catch

--- src/controllers/cryptowat.controller.ts
+++ src/controllers/cryptowat.controller.ts
@@ -8,27 +8,35 @@ const PAIR = "btcusd";
 const INTERVAL = 3600;
 
 export const latestController = async (req: Request, res: Response) => {
-  const end = parseInt((new Date().getTime() / 1000).toFixed(0));
-  const start = end - 86400;
-  const latest = `https://api.cryptowat.ch/markets/${EXCHANGE}/${PAIR}/ohlc?after=${start}&before=${end}&periods=${INTERVAL}`;
+  try {
+    const end = parseInt((new Date().getTime() / 1000).toFixed(0));
+    const start = end - 86400;
+    const latest = `https://api.cryptowat.ch/markets/${EXCHANGE}/${PAIR}/ohlc?after=${start}&before=${end}&periods=${INTERVAL}`;
 
-  const apiRes = await nFetch(latest);
-  const jsonData = (await apiRes.json()) as Cryptowat;
-  console.log("API Allowance Remaining", jsonData.allowance.remaining);
-  const formatted = processResult(jsonData.result, INTERVAL);
+    const apiRes = await nFetch(latest);
+    const jsonData = (await apiRes.json()) as Cryptowat;
+    console.log("API Allowance Remaining", jsonData.allowance.remaining);
+    const formatted = processResult(jsonData.result, INTERVAL);
 
-  res.send(formatted);
+    res.send(formatted);
+  } catch (error) {
+    res.status(503).send(error);
+  }
 };
 
 export const historicController = async (req: Request, res: Response) => {
-  const start = req.query.start;
-  const end = req.query.end;
-  const history = `https://api.cryptowat.ch/markets/${EXCHANGE}/${PAIR}/ohlc?after=${start}&before=${end}&periods=${INTERVAL}`;
+  try {
+    const start = req.query.start;
+    const end = req.query.end;
+    const history = `https://api.cryptowat.ch/markets/${EXCHANGE}/${PAIR}/ohlc?after=${start}&before=${end}&periods=${INTERVAL}`;
 
-  const apiRes = await nFetch(history);
-  const jsonData = (await apiRes.json()) as Cryptowat;
-  console.log("API Allowance Remaining", jsonData.allowance.remaining);
-  const formatted = processResult(jsonData.result, INTERVAL);
+    const apiRes = await nFetch(history);
+    const jsonData = (await apiRes.json()) as Cryptowat;
+    console.log("API Allowance Remaining", jsonData.allowance.remaining);
+    const formatted = processResult(jsonData.result, INTERVAL);
 
-  res.send(formatted);
+    res.send(formatted);
+  } catch (error) {
+    res.status(503).send(error);
+  }
 };
