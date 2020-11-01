a4e06c678f5418114fa022adfdb24262a24d8177
Durrani <baharalidurrani@gmail.com>
2020-11-02 00:09:47 +0500

    dynamic interval added

--- src/controllers/cryptowat.controller.ts
+++ src/controllers/cryptowat.controller.ts
@@ -7,10 +7,32 @@ const EXCHANGE = "kraken";
 const PAIR = "btcusd";
 const INTERVAL = 3600;
 
+const CRYPTOWATCH_INTERVALS = [
+  60,
+  180,
+  300,
+  900,
+  1800,
+  3600,
+  7200,
+  14400,
+  21600,
+  43200,
+  86400,
+  259200,
+  604800
+];
+const optimalInterval = (start: number, end: number) => {
+  const goal = (end - start) / 50;
+  return CRYPTOWATCH_INTERVALS.reduce((prev, curr) => {
+    return Math.abs(curr - goal) < Math.abs(prev - goal) ? curr : prev;
+  });
+};
+
 export const latestController = async (req: Request, res: Response) => {
   try {
     const end = parseInt((new Date().getTime() / 1000).toFixed(0));
-    const start = end - 86400;
+    const start = end - 86400; //24hrs
     const latest = `https://api.cryptowat.ch/markets/${EXCHANGE}/${PAIR}/ohlc?after=${start}&before=${end}&periods=${INTERVAL}`;
 
     const apiRes = await nFetch(latest);
@@ -20,23 +42,27 @@ export const latestController = async (req: Request, res: Response) => {
 
     res.send(formatted);
   } catch (error) {
-    res.status(503).send(error);
+    console.log(error);
+    res.status(503).send("Problem while fetching data from CryptoWatch API!");
   }
 };
 
 export const historicController = async (req: Request, res: Response) => {
   try {
-    const start = req.query.start;
-    const end = req.query.end;
-    const history = `https://api.cryptowat.ch/markets/${EXCHANGE}/${PAIR}/ohlc?after=${start}&before=${end}&periods=${INTERVAL}`;
+    const start = req.query.start as string;
+    const end = req.query.end as string;
+    const interval =
+      optimalInterval(parseInt(start), parseInt(end)) || INTERVAL;
+    const history = `https://api.cryptowat.ch/markets/${EXCHANGE}/${PAIR}/ohlc?after=${start}&before=${end}&periods=${interval}`;
 
     const apiRes = await nFetch(history);
     const jsonData = (await apiRes.json()) as Cryptowat;
     console.log("API Allowance Remaining", jsonData.allowance.remaining);
-    const formatted = processResult(jsonData.result, INTERVAL);
+    const formatted = processResult(jsonData.result, interval);
 
     res.send(formatted);
   } catch (error) {
-    res.status(503).send(error);
+    console.log(error);
+    res.status(503).send("Problem while fetching data from CryptoWatch API!");
   }
 };
